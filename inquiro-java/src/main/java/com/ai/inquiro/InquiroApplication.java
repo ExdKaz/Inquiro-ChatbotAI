620a4cf043cf2d38423313bbe4735159ba345f08
abhishek <mrabhishekrana123@gmail.com>
2025-07-25 13:39:20 +0530

    java first commit

---
+++ inquiro-java/src/main/java/com/ai/inquiro/InquiroApplication.java
@@ -0,0 +1,13 @@
+package com.ai.inquiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InquiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InquiroApplication.class, args);
+	}
+
+}
---
+++ inquiro-java/src/test/java/com/ai/inquiro/InquiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.inquiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InquiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
